52a6d5bf48f39b423e18edc6744d5c8a087f917a
frappe <frappe@ip-172-31-5-249.me-south-1.compute.internal>
2022-01-11 09:06:58 +0000

    customize in QI

--- tsl/custom_py/quotation.py
+++ tsl/custom_py/quotation.py
@@ -7,3 +7,13 @@ def on_update(self, method):
 			self.workflow_state = "Waiting For Approval"
 		else:
 			frappe.db.set_value(self.doctype, self.name, "workflow_state", "Quoted to Customer")
+
+	if not self.quotation_type == "Internal Quotation":
+		for i in self.items:
+			if i.prevdoc_doctype == "Work Order Data" and i.prevdoc_docname:
+				if frappe.db.get_value(self.doctype, self.name, "workflow_state") == "Quoted to Customer":
+					frappe.db.set_value("Work Order Data", i.prevdoc_docname, "status", "Q-Quoted")
+				if frappe.db.get_value(self.doctype, self.name, "workflow_state") == "Approved By Customer":
+					frappe.db.set_value("Work Order Data", i.prevdoc_docname, "status", "A-Approved")
+				if frappe.db.get_value(self.doctype, self.name, "workflow_state") == "Rejected by Customer":
+					frappe.db.set_value("Work Order Data", i.prevdoc_docname, "status", "RNA-Return Not Approved")
