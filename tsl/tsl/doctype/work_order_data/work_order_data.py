e18d43cfd955260341c99d6a48a8f700879d98a8
MYE-12 <mohamedyousufesi46@gmail.com>
2023-07-03 10:09:19 +0300

    Quotation

--- tsl/tsl/doctype/work_order_data/work_order_data.py
+++ tsl/tsl/doctype/work_order_data/work_order_data.py
@@ -35,6 +35,7 @@ def create_quotation(wod):
 	new_doc= frappe.new_doc("Quotation")
 	new_doc.company = doc.company
 	new_doc.party_name = doc.customer
+	new_doc.customer_email = doc.customer_email
 	# new_doc.party_name = new_doc.party_name[0]
 	new_doc.department = doc.department
 	new_doc.currency = frappe.db.get_value("Company",doc.company,"default_currency")
