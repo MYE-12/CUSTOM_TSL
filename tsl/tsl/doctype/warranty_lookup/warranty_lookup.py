94c0a95920bbeb341df59b50a37e6603678e7731
MYE-12 <mohamedyousufesi46@gmail.com>
2023-09-02 18:33:08 +0530

    Warranty DN

--- tsl/tsl/doctype/warranty_lookup/warranty_lookup.py
+++ tsl/tsl/doctype/warranty_lookup/warranty_lookup.py
@@ -13,5 +13,4 @@ def get_dn(wod):
 	delivery_note = frappe.db.sql("""select `tabDelivery Note Item`.wod_no as wo,`tabDelivery Note`.warranty_end_date,`tabDelivery Note`.posting_date as date,`tabDelivery Note`.name as dn from `tabDelivery Note`
 		left join `tabDelivery Note Item` on `tabDelivery Note`.name = `tabDelivery Note Item`.parent
 		where `tabDelivery Note Item`.wod_no = '%s' and status != 'Cancelled' """ % (wod), as_dict=True)
-
 	return delivery_note
