f806eba2befee87016740f39448f91bf9820457f
Ubuntu <ubuntu@ip-172-31-46-238.eu-north-1.compute.internal>
2025-08-04 10:57:05 +0000

    setup

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in tsl/__init__.py
-# from tsl import __version__ as version
+from tsl import __version__ as version
 
-# setup(
-# 	name="tsl",
-# 	version=version,
-# 	description="Tsl",
-# 	author="Tsl",
-# 	author_email="Tsl",
-# 	packages=find_packages(),
-# 	zip_safe=False,
-# 	include_package_data=True,
-# 	install_requires=install_requires
-# )
+setup(
+	name="tsl",
+	version=version,
+	description="Tsl",
+	author="Tsl",
+	author_email="Tsl",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	# install_requires=install_requires
+)
