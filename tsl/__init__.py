85880ca340b16ec0193746609d4ee7e47a7f189a
Ubuntu <ubuntu@ip-172-31-46-238.eu-north-1.compute.internal>
2025-08-04 11:02:53 +0000

    setup

--- tsl/__init__.py
+++ tsl/__init__.py
@@ -1,7 +1,7 @@
 
-# __version__ = '0.0.1'
-# import frappe
-# from frappe.model.mapper import get_mapped_doc
+__version__ = '0.0.1'
+import frappe
+from frappe.model.mapper import get_mapped_doc
 
 # @frappe.whitelist()
 # def get_revised_po(source):
@@ -36,29 +36,29 @@
 # 	return doclist
 
 
-# # @frappe.whitelist()
-# # def create_workorder_data(order_no):
-# # 	l=[]
-# # 	if frappe.db.exists("Work Order Data",{"reference_doctype":order_no}):
-# # 		frappe.throw("Order Already Created")
-# # 	doc = frappe.get_doc("Equipment Received Form",order_no)
-# # 	for i in doc.get("received_equipment"):
-# # 		new_doc = frappe.new_doc("Work Order Data")
-# # 		new_doc.customer = doc.customer
-# # 		new_doc.quoted_date = doc.date
-# # 		new_doc.sales_rep = doc.incharge
-# # 		new_doc.equipment_recieved_form = doc.name
-# # 		new_doc.append("material_list",{
-# # 			"item":i.item,
-# # 			"mfg":i.manufacturer,
-# # 			"serial_no":i.serial_no,
-# # 			"quantity":i.qty
-# # 		})
-# # 		new_doc.save(ignore_permissions = True)
-# # 		l.append(new_doc.name)
-# # 	if l:
-# # 		return True
-# # 	return False
+# @frappe.whitelist()
+# def create_workorder_data(order_no):
+# 	l=[]
+# 	if frappe.db.exists("Work Order Data",{"reference_doctype":order_no}):
+# 		frappe.throw("Order Already Created")
+# 	doc = frappe.get_doc("Equipment Received Form",order_no)
+# 	for i in doc.get("received_equipment"):
+# 		new_doc = frappe.new_doc("Work Order Data")
+# 		new_doc.customer = doc.customer
+# 		new_doc.quoted_date = doc.date
+# 		new_doc.sales_rep = doc.incharge
+# 		new_doc.equipment_recieved_form = doc.name
+# 		new_doc.append("material_list",{
+# 			"item":i.item,
+# 			"mfg":i.manufacturer,
+# 			"serial_no":i.serial_no,
+# 			"quantity":i.qty
+# 		})
+# 		new_doc.save(ignore_permissions = True)
+# 		l.append(new_doc.name)
+# 	if l:
+# 		return True
+# 	return False
 
 
 	
